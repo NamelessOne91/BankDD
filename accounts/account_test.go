810656a357c6d54b5da3c4d947f5c7916aacc546
Paolo Invernizzi <paolo.invernizzi@lightcast.com>
2023-10-21 21:11:26 +0200

    Add table tests for sequential calls to Deposit

--- accounts/account_test.go
+++ accounts/account_test.go
@@ -2,6 +2,7 @@ package accounts_test
 
 import (
 	"bankdd/accounts"
+	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -64,5 +65,29 @@ var _ = Describe("Account", func() {
 			_, err := account.Transfer(nil, 100*accounts.Euro)
 			Expect(err).To(Equal(accounts.ErrorInactiveAccount))
 		})
+	})
+
+	Describe("can deposit money to an open account", func() {
+
+		DescribeTable("correctly updates the account's balance after a deposit",
+			func(start accounts.Currency, deposits []accounts.Currency, expectedBalance accounts.Currency) {
+				var account = accounts.NewBankAccount()
+				Expect(account.Open(start)).To(Succeed())
+
+				for _, amount := range deposits {
+					_, err := account.Deposit(amount)
+					Expect(err).To(BeNil())
+				}
+				Expect(account.Balance()).To(Equal(expectedBalance))
+			},
+			func(start accounts.Currency, deposits []accounts.Currency, expectedBalance accounts.Currency) string {
+				return fmt.Sprintf("start: %d - deposits: %v - expected balance: %d", start, deposits, expectedBalance)
+			},
+			Entry(nil, 0*accounts.Euro, []accounts.Currency{}, 0*accounts.Euro),
+			Entry(nil, 0*accounts.Euro, []accounts.Currency{100 * accounts.Euro}, 100*accounts.Euro),
+			Entry(nil, 0*accounts.Euro, []accounts.Currency{100 * accounts.Euro, 30 * accounts.Cent}, (100*accounts.Euro+30*accounts.Cent)),
+			Entry(nil, 1000*accounts.Euro, []accounts.Currency{150 * accounts.Euro, 50 * accounts.Cent, 720 * accounts.Euro}, (1870*accounts.Euro+50*accounts.Cent)),
+			Entry(nil, 1000*accounts.Euro, []accounts.Currency{150 * accounts.Euro, 50 * accounts.Cent, 720 * accounts.Euro, 3 * accounts.Cent}, (1870*accounts.Euro+53*accounts.Cent)),
+		)
 	})
 })
