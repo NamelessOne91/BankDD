e820b9145b93c737880189e53abf592cc74d4d7c
Paolo Invernizzi <paolo.invernizzi@suse.com>
2023-11-10 19:14:30 +0100

    Add features for open account failures + tests

--- accounts/accounts_cucumber_test.go
+++ accounts/accounts_cucumber_test.go
@@ -12,6 +12,11 @@ import (
 
 type accountCtxKey struct{}
 
+type accountCtx struct {
+	account *BankAccount
+	err     error
+}
+
 func TestFeatures(t *testing.T) {
 	suite := godog.TestSuite{
 		ScenarioInitializer: InitializeScenario,
@@ -28,64 +33,113 @@ func TestFeatures(t *testing.T) {
 }
 
 func InitializeScenario(sc *godog.ScenarioContext) {
-	sc.Given(`^I have a bank account$`, IHaveABankAccount)
-	sc.When(`^I open it with a starting sum of (\d+) euro$`, IOpenWithStartingEuro)
+	sc.Given(`^I have a new bank account$`, IHaveANewBankAccount)
+	sc.Given(`^I have an active bank account$`, IHaveAnActiveBankAccount)
+	sc.When(`^I open it with a starting sum of (-?\d+) euro$`, IOpenWithStartingEuro)
 	sc.Then(`^it should have a balance of (\d+) euro$`, ItShouldHaveABalanceOf)
 	sc.Then(`^it should be an active account$`, ItShouldBeActive)
 	sc.Then(`^it should have today as opening date$`, ItShouldHaveTodayAsOpenDate)
+	sc.Then(`^I should get an invalid amount error$`, IShouldGetInvalidAmountError)
+	sc.Then(`^I should get an error because the account is already open$`, IShouldGetAlreadyOpenError)
 }
 
-func IHaveABankAccount(ctx context.Context) (context.Context, error) {
+func IHaveANewBankAccount(ctx context.Context) (context.Context, error) {
 	account := NewBankAccount()
-	return context.WithValue(ctx, accountCtxKey{}, account), nil
+	return context.WithValue(ctx, accountCtxKey{}, &accountCtx{account: account}), nil
+}
+
+func IHaveAnActiveBankAccount(ctx context.Context) (context.Context, error) {
+	account := NewBankAccount()
+	account.active = true
+	return context.WithValue(ctx, accountCtxKey{}, &accountCtx{account: account}), nil
 }
 
 func IOpenWithStartingEuro(ctx context.Context, euro int) (context.Context, error) {
-	account, ok := ctx.Value(accountCtxKey{}).(*BankAccount)
+	accountCtx, ok := ctx.Value(accountCtxKey{}).(*accountCtx)
 	if !ok {
 		return ctx, errors.New("no bank account available")
 	}
 
-	if err := account.Open(Euro * Currency(euro)); err != nil {
-		return ctx, fmt.Errorf("got an error opening bank account: %s", err)
+	if err := accountCtx.account.Open(Euro * Currency(euro)); err != nil {
+		if euro > 0 && !accountCtx.account.active {
+			return ctx, err
+		}
+		accountCtx.err = err
 	}
 	return ctx, nil
 }
 
 func ItShouldHaveABalanceOf(ctx context.Context, euro int) error {
-	account, ok := ctx.Value(accountCtxKey{}).(*BankAccount)
+	accountCtx, ok := ctx.Value(accountCtxKey{}).(*accountCtx)
 	if !ok {
 		return errors.New("no bank account available")
 	}
+	if accountCtx.err != nil {
+		return fmt.Errorf("error in previous step: %s", accountCtx.err)
+	}
 
 	expectedBalance := Euro * Currency(euro)
-	if account.balance != expectedBalance {
-		return fmt.Errorf("expected account's balance to be %d - got %d", expectedBalance, account.balance)
+	if accountCtx.account.balance != expectedBalance {
+		return fmt.Errorf("expected account's balance to be %d - got %d", expectedBalance, accountCtx.account.balance)
 	}
 	return nil
 }
 
 func ItShouldBeActive(ctx context.Context) error {
-	account, ok := ctx.Value(accountCtxKey{}).(*BankAccount)
+	accountCtx, ok := ctx.Value(accountCtxKey{}).(*accountCtx)
 	if !ok {
 		return errors.New("no bank account available")
 	}
+	if accountCtx.err != nil {
+		return fmt.Errorf("error in previous step: %s", accountCtx.err)
+	}
 
-	if !account.active {
+	if !accountCtx.account.active {
 		return errors.New("expected the account to be flagged as active but it is not")
 	}
 	return nil
 }
 
 func ItShouldHaveTodayAsOpenDate(ctx context.Context) error {
-	account, ok := ctx.Value(accountCtxKey{}).(*BankAccount)
+	accountCtx, ok := ctx.Value(accountCtxKey{}).(*accountCtx)
 	if !ok {
 		return errors.New("no bank account available")
 	}
+	if accountCtx.err != nil {
+		return fmt.Errorf("error in previous step: %s", accountCtx.err)
+	}
 
 	today := time.Now().Truncate(time.Minute)
-	if account.openDate.Truncate(time.Minute).Compare(today) != 0 {
-		return fmt.Errorf("expected account's open date to be %s - got %s", today.String(), account.openDate.Truncate(time.Minute).String())
+	if accountCtx.account.openDate.Truncate(time.Minute).Compare(today) != 0 {
+		return fmt.Errorf(
+			"expected account's open date to be %s - got %s",
+			today.String(),
+			accountCtx.account.openDate.Truncate(time.Minute).String(),
+		)
 	}
+	return nil
+}
+
+func IShouldGetInvalidAmountError(ctx context.Context) error {
+	accountCtx, ok := ctx.Value(accountCtxKey{}).(*accountCtx)
+	if !ok {
+		return errors.New("no bank account available")
+	}
+	if accountCtx.err != ErrorInvalidAmount {
+		return fmt.Errorf("expected error in previous step to be %s - got %s", ErrorInvalidAmount, accountCtx.err)
+	}
+
+	return nil
+}
+
+func IShouldGetAlreadyOpenError(ctx context.Context) error {
+	accountCtx, ok := ctx.Value(accountCtxKey{}).(*accountCtx)
+	if !ok {
+		return errors.New("no bank account available")
+	}
+	if accountCtx.err != ErrorAccountIsOpen {
+		return fmt.Errorf("expected error in previous step to be %s - got %s", ErrorAccountIsOpen, accountCtx.err)
+	}
+
 	return nil
 }
--- accounts/features/accounts.feature
+++ accounts/features/accounts.feature
@@ -4,8 +4,23 @@ Feature: manage bank account
     I need to be able to execute operations on my bank account
 
     Scenario: Open Bank account
-        Given I have a bank account
+        Given I have a new bank account
         When I open it with a starting sum of 1000 euro
         Then it should have a balance of 1000 euro 
         And it should be an active account
         And it should have today as opening date
+    
+    Scenario: Open Bank Account with amount = 0
+        Given I have a new bank account
+        When I open it with a starting sum of 0 euro
+        Then I should get an invalid amount error
+    
+    Scenario: Open Bank Account with negative amount
+        Given I have a new bank account
+        When I open it with a starting sum of -1000 euro
+        Then I should get an invalid amount error
+    
+    Scenario: Open Bank Account which is already active
+        Given I have an active bank account
+        When I open it with a starting sum of 1000 euro
+        Then I should get an error because the account is already open
